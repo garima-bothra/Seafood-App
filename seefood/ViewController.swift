08c7601af8302b1af680bcdea44898335994fc48
garima94921 <gaarimabothra@gmail.com>
2019-11-28 17:46:58 +0530

    First commit

--- seefood/ViewController.swift
+++ seefood/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+             imageView.image = userPickedImage
+            
+            guard  let ciimage = CIImage(image: userPickedImage)else {
+                fatalError("Error converting to CIImage")
+            }
+            
+            detect(image: ciimage)
+            
+        }
+       
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image : CIImage)
+    {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model)else {
+            fatalError("Loading COREML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("Model failed to process error")
+            }
+            
+            
+            if let firstResult = results.first{
+                if firstResult.identifier.contains("keyboard"){
+                    self.navigationItem.title = "KEYBOARD!"
+                }
+                else{
+                    self.navigationItem.title = "NOT KEYBOARD!"
+                }
+            }
+        
+        
+        }
+        
+        let handler = VNImageRequestHandler(ciImage : image)
+        do{
+        try handler.perform([request])
+        }
+        catch{
+            print(error)
+        }
+    }
+    
+    let imagePicker = UIImagePickerController()
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
